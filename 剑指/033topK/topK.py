9f29b2cab0405d552807851e8dfad67c5c086029
alexzsh <zjutzsh@gmail.com>
2019-03-05 23:00:45 +0800

    updatedaily

--- 剑指/033topK/topK.py
+++ 剑指/033topK/topK.py
@@ -1,53 +1,31 @@
-#-*- coding:utf-8 -*-
-#author: zsh
-#modified by zsh at date
-#version:
-#history:
-#relate on:
-import heapq
-class topKSmall(object):
-    def __init__(self, k):
-        self.k = k
-        self.data = []
-    def push(self,data):
-        if len(self.data)<self.k:
-            heapq.heappush(self.data,data)
-            return None
-        else:
-            topK = self.data[0]
-            if data>topK:
-                return heapq.heapreplace(self.data,data)
-    def getSmall(self):
-        return self.data[0]
-class topKLarge(object):
-    def __init__(self, k):
-        self.k = k
-        self.data = []
-    
-    def push(self,data):
-        if len(self.data)<self.k:
-            heapq.heappush(self.data,data)
-        else:
-            topK = self.data[0]
-            if data>topK:
-                heapq.heapreplace(self.data,data)
-    def topK(self):
-        return [x for x in reversed([heapq.heappop(self.data) for x in range(len(self.data))])]
-    def getLarge(self):
-        return -self.data[0] 
-if __name__ == '__main__':
-    listNum=[1,1,1,1,2,3,4,5,6,7,8,9]
-    small = len(listNum)>>1
-    large = len(listNum)-small
-    thSmall = topKSmall(small)
-    thLarge = topKLarge(large)
-    for i in listNum:
-        res = thSmall.push(i)
-        if res==None:
-            continue
-        else:
-            thLarge.push(-res)
-    res = thLarge.getLarge() if len(listNum)%2!=0 else (thLarge.getLarge()+thSmall.getSmall())/2
-    print(res)
+import random
 
-    
+
+class Solution:
+    def partrition(self, num, start, end):
+        mid = start+(end-start) >> 1
+        index = random.randint(start, end)
+        change = start-1
+        num[index], num[end] = num[end], num[index]
+        for i in range(start, end):
+            if num[i] < num[end]:
+                change += 1
+                num[i], num[change] = num[change], num[i]
+        change += 1
+        num[change], num[end] = num[end], num[change]
+        return change
+
+    def GetLeastNumbers_Solution(self, tinput, k):
+        # write code here
+        if tinput == None or k > len(tinput) or k == 0:
+            return []
+        start = 0
+        end = len(tinput)-1
+        index = self.partrition(tinput, start, end)
+        while index != k-1:
+            if index > k-1:
+                end = index-1
+            else:
+                start = index+1
+            index = self.partrition(tinput, start, end)
+        return sorted(tinput[:k])
